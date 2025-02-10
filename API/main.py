4c69260fe579a39317850fc9468c5d6f594a96c0
izma26 <ismaeltir14@gmail.com>
2025-02-10 13:50:50 +0100

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,32 +1,47 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, redirect, url_for, session
 from data import Model
 
+# Initialisation du modèle
 model = Model()
 
 app = Flask(__name__)
- 
-# Set a secret key for encrypting session data
+
+# Clé secrète pour Flask (nécessaire pour gérer les sessions)
 app.secret_key = 'my_secret_key'
- 
- 
-# To render a login form 
+
+# Route pour afficher le formulaire
 @app.route('/')
 def view_form():
-    return render_template('predict.html')
- 
-# Send prediction result
-@app.route('/predict', methods=['POST'])
+    return render_template('predict.html', show_form=True)
+
+# Route pour obtenir la prédiction et rediriger vers /predict
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
-        sepal_length = request.form['sepal_length']
-        sepal_width = request.form['sepal_width']
-        petal_length = request.form['petal_length']
-        petal_width = request.form['petal_width']
-        print(petal_length, petal_width)
-        
-        return {"response":model.predict(sepal_length, sepal_width, petal_length, petal_width)[0]}
-    else:
-        return render_template('predict.html')
- 
+        try:
+            # Récupération des valeurs saisies par l'utilisateur
+            sepal_length = float(request.form['sepal_length'])
+            sepal_width = float(request.form['sepal_width'])
+            petal_length = float(request.form['petal_length'])
+            petal_width = float(request.form['petal_width'])
+
+            # Prédiction avec le modèle
+            prediction = model.predict(sepal_length, sepal_width, petal_length, petal_width)
+
+            # Stockage du résultat dans la session pour l'afficher après redirection
+            session['prediction'] = prediction
+            return {"response" : prediction}
+            #redirect(url_for('predict'))
+
+
+
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+    elif request.method == 'GET':
+        # Vérifie s'il y a une prédiction stockée et l'affiche sans formulaire
+        prediction = session.get('prediction', None)
+        return render_template('predict.html', prediction=prediction, show_form=False)
+
 if __name__ == '__main__':
     app.run()

eb911a681d401b086afc7227d75d178f4c5a706b
izma26 <ismaeltir14@gmail.com>
2025-02-10 13:51:39 +0100

    Update data.py

--- API/data.py
+++ API/data.py
@@ -1,51 +1,47 @@
-import pandas as pd
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from flask import Flask, request, render_template, jsonify, redirect, url_for, session
+from data import Model
 
-class Model :
 
-    model : DecisionTreeClassifier
-    
-    def __init__(self) :
+model = Model()
+
+app = Flask(__name__)
+
+
+app.secret_key = 'my_secret_key'
+
+
+@app.route('/')
+def view_form():
+    return render_template('predict.html', show_form=True)
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        try:
+           
+            sepal_length = float(request.form['sepal_length'])
+            sepal_width = float(request.form['sepal_width'])
+            petal_length = float(request.form['petal_length'])
+            petal_width = float(request.form['petal_width'])
+
+          
+            prediction = model.predict(sepal_length, sepal_width, petal_length, petal_width)
+
+            
+            session['prediction'] = prediction
+            return {"response" : prediction}
+            #redirect(url_for('predict'))
+
+
+
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+    elif request.method == 'GET':
         
-        # 1. Load from sklean.datasets the Iris dataset after importing load iris data object and 
-        # use a variable-name iris to store the returned value: iris = load iris();
+        prediction = session.get('prediction', None)
+        return render_template('predict.html', prediction=prediction, show_form=False)
 
-        self.iris = load_iris()
-
-        # 2. Extract from iris.data the petal length and width then affect them to an 
-        # input data as a new variable X
-
-        self.iris_df = pd.DataFrame(self.iris.data, columns=["Sepal_Length_cm",
-                                                    "Sepal_Width_cm",
-                                                    "Petal_Length_cm",
-                                                        "Petal_Width_cm"])
-
-        X = self.iris_df[["Sepal_Length_cm", "Sepal_Width_cm", "Petal_Length_cm", "Petal_Width_cm"]]
-        Y = self.iris.target
-
-        print(self.iris.target)
-
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
-
-        # 4. Import DecisionTreeClassifier from sklearn.tree and create a new object treeClassifer 
-        # with a maximum depth value equal to 2.
-
-        self.model = DecisionTreeClassifier(max_depth=2)
-
-        # 5. Fit your decision tree on your data
-
-        self.model.fit(X_train, y_train)
-
-        # 6. We test the model accuracy
-        y_pred = self.model.predict(X_test)
-
-        print(f"Accuracy : {accuracy_score(y_test, y_pred)}")
-
-    def predict(self, sepal_length, sepal_width, petal_length, petal_width) :
-
-        return self.iris.target_names[self.model.predict([[sepal_length, sepal_width, petal_length, petal_width]])]
-    
+if __name__ == '__main__':
+    app.run()
